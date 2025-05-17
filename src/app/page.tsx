128edd23e84092141d052d57326d6d7ff337fe4c
gsp105 <gsp105@gsp.local>
2025-05-17 16:16:03 +0200

    Descripción de los cambios realizados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import TodoList from "@/components/todo-list"
+﻿import TodoList from "@/components/todo-list"
 
 export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-4 bg-gray-50">
-      <div className="w-full max-w-md">
-        <TodoList />
-      </div>
-    </main>
-  )
+    return (
+        <main className="min-h-screen flex flex-col items-center justify-center p-4 bg-gray-50">
+            <div className="w-full max-w-md">
+                <TodoList />
+            </div>
+        </main>
+    )
 }
